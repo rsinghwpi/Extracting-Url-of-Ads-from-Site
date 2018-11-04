c9ed9e522ffd97438beeef957d0cd6baccb64101
rsinghwpi <44755650+rsinghwpi@users.noreply.github.com>
2018-11-04 16:09:06 -0500

    Update scrap_data.py

--- scrap_data.py
+++ scrap_data.py
@@ -14,3 +14,18 @@ def write_data_to_csv(data):
     with open("D:/Python_CSV/phones_output1.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(data)
+# read the saved urls file as a dataframe 
+urls_data = pd.read_csv("D:/Python_CSV/urls.csv")
+# create  a list that will hold the final data
+final_result = []
+i = 1
+# loop over the dataframe
+for index, row in urls_data.iterrows():
+    final_result.append(scrap_ad_data(row['url']))
+    # to count how many page we have processed since qe have 35 links per page
+    #i += 1
+    #if i%100 == 0:
+    #    print("page ",i, "done")
+print('Scrapping data finished')
+# now that we have all the data we can write it in a csv file
+write_data_to_csv(final_result)
